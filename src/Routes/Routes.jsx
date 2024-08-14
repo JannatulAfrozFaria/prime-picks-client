e25a7e3d97e010660a7fc3a0ed00273a3668f27a
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-08-15 02:43:43 +0600

    home page created

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,10 +3,17 @@ import {
     // RouterProvider,
   } from "react-router-dom";
 import Main from "../Layout/Main";
+import Home from "../pages/Home/Home";
 
   export const router = createBrowserRouter([
     {
       path: "/",
       element: <Main></Main> ,
+      children: [
+        {
+            path: '/',
+            element: <Home></Home>
+        }
+      ]
     },
   ]);
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+        <div>
+            This is a Pharma website
+        </div>
+    );
+};
+
+export default Home;

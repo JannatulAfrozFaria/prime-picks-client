084460c2c45dd2961bd8059479575ee71b023c18
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-08-16 05:50:53 +0600

    filter by price range option created in medicine section

--- src/pages/Home/MedicineSection.jsx
+++ src/pages/Home/MedicineSection.jsx
@@ -65,7 +65,7 @@ const MedicineSection = () => {
         <div className="mb-8">
             <Title heading={'Health Essentials'} subHeading={'Here comes our wide range of collection'}></Title>
             {/* Search,Filter Section */}
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-1 my-4">
+            <div className="grid grid-cols-1 md:grid-cols-5 gap-1 my-4">
                 {/* SEARCH----OPTION */}
                 <div className="join">
                     <div className=''>
@@ -83,14 +83,13 @@ const MedicineSection = () => {
                         <details className="dropdown">
                             <summary className="btn btn-filter w-28 md:w-full">Filter By Brand <IoIosArrowDown /></summary>
                             <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
-                                <li><Link onClick={()=>handleFilterByBrand('AllergyGuard')}>AllergyGuard</Link></li>
                                 <li><Link onClick={()=>handleFilterByBrand('AntibioticPlus')}>AntibioticPlus</Link></li>
-                                <li><Link onClick={()=>handleFilterByBrand('DigestWell')}>DigestWell</Link></li>
+                                <li><Link onClick={()=>handleFilterByBrand('BioPharma')}>BioPharma</Link></li>
                                 <li><Link onClick={()=>handleFilterByBrand('GlucoCare')}>GlucoCare</Link></li>
                                 <li><Link onClick={()=>handleFilterByBrand('HealthCare')}>HealthCare</Link></li>
-                                <li><Link onClick={()=>handleFilterByBrand('HeartGuard')}>HeartGuard</Link></li>
                                 <li><Link onClick={()=>handleFilterByBrand('HerbHealth')}>HerbHealth</Link></li>
                                 <li><Link onClick={()=>handleFilterByBrand('NutriBoost')}>NutriBoost</Link></li>
+                                <li><Link onClick={()=>handleFilterByBrand('Square')}>Square</Link></li>
                             </ul>
                         </details>
                 </div>   
@@ -98,6 +97,22 @@ const MedicineSection = () => {
                 <div>
                         <details className="dropdown">
                             <summary className="btn btn-filter w-28 md:w-full">Filter By Category <IoIosArrowDown /></summary>
+                            <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+                                <li><Link onClick={()=>handleFilterByCategory('Allergy Relief')}>Allergy Relief</Link></li>
+                                <li><Link onClick={()=>handleFilterByCategory('Antibiotics')}>Antibiotics</Link></li>
+                                <li><Link onClick={()=>handleFilterByCategory('Diabetes Care')}>Diabetes Care</Link></li>
+                                <li><Link onClick={()=>handleFilterByCategory('Digestive Health')}>Digestive Health</Link></li>
+                                <li><Link onClick={()=>handleFilterByCategory('Heart Health')}>Heart Health</Link></li>
+                                <li><Link onClick={()=>handleFilterByCategory('Herbal Remedies')}>Herbal Remedies</Link></li>
+                                <li><Link onClick={()=>handleFilterByCategory('Pain Reliever')}>Pain Reliever</Link></li>
+                                <li><Link onClick={()=>handleFilterByCategory('Supplements')}>Supplements</Link></li>
+                            </ul>
+                        </details>
+                </div>   
+                {/* FILTER---BY---PRICE---RANGE---OPTION */}
+                <div>
+                        <details className="dropdown">
+                            <summary className="btn btn-filter w-28 md:w-full">Filter By Price <IoIosArrowDown /></summary>
                             <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
                                 <li><Link onClick={()=>handleFilterByCategory('Allergy Relief')}>Allergy Relief</Link></li>
                                 <li><Link onClick={()=>handleFilterByCategory('Antibiotics')}>Antibiotics</Link></li>

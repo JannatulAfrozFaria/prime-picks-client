a6afd395cd9ba45d3d035092a1bc1c7554d0eb40
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-08-17 22:56:40 +0600

    medicine section made responsive

--- src/pages/Home/MedicineSection.jsx
+++ src/pages/Home/MedicineSection.jsx
@@ -121,31 +121,31 @@ const MedicineSection = () => {
     console.log(searchResults);
     if(!searchResults.items){
 
-        return <div>Loading</div>
+        return <progress className="progress progress-success w-56" value="40" max="100"></progress>
     }
     return (
         <div className="mb-8">
             <Title heading={'Health Essentials'} subHeading={'Here comes our wide range of collection'}></Title>
             {/* Search,Filter Section */}
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-2 mt-4 mb-8 w-full md:w-3/4 mx-auto items-center justify-center">
+            <div className="grid grid-cols-1 md:grid-cols-5 gap-1 mt-4 mb-8 w-full md:w-3/4 mx-auto items-center justify-center">
                 {/* SEARCH----OPTION */}
-                <div className="join w-1/3 md:w-full mx-auto">
-                    <div>
+                <div className="join w-1/3 md:w-full mx-auto col-span-1">
+                    <div className="w-full">
                         <input type="text" value={searchTerm}
                         onChange={handleChange} 
-                        className="input input-bordered join-item w-24 md:w-44" placeholder="Search"/>
+                        className="input input-bordered join-item w-full" placeholder="Search"/>
                     </div>
-                    <div className="indicator">
-                        <button onClick={handleSearch} className="btn join-item btn-filter w-full md:w-36">Search</button>
+                    <div className="indicator w-full">
+                        <button onClick={handleSearch} className="btn join-item btn-filter w-full">Search</button>
                     </div>
                 </div>
                 {/* division---1 */}
-                <div className="grid grid-cols-2 items-center justify-center w-2/3 md:w-full mx-auto">
+                <div className="grid grid-cols-2 gap-1 items-center justify-center w-2/3 md:w-full mx-auto col-span-2">
                      {/* FILTER---BY---BRAND---OPTION */}
-                    <div>
-                        <details className="dropdown">
-                            <summary className="btn btn-filter w-40 md:w-full">Filter By Brand <IoIosArrowDown /></summary>
-                            <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+                    <div className="w-full">
+                        <details className="dropdown w-full">
+                            <summary className="btn btn-filter w-full">Filter By Brand <IoIosArrowDown /></summary>
+                            <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box">
                                 <li><Link onClick={()=>handleFilterByBrand('AntibioticPlus')}>AntibioticPlus</Link></li>
                                 <li><Link onClick={()=>handleFilterByBrand('BioPharma')}>BioPharma</Link></li>
                                 <li><Link onClick={()=>handleFilterByBrand('GlucoCare')}>GlucoCare</Link></li>
@@ -157,10 +157,10 @@ const MedicineSection = () => {
                         </details>
                     </div>   
                     {/* FILTER---BY---CATEGORY---OPTION */}
-                    <div>
-                        <details className="dropdown">
-                            <summary className="btn btn-filter w-44 md:w-42">FilterByCategory<IoIosArrowDown /></summary>
-                            <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+                    <div className="w-full">
+                        <details className="dropdown w-full">
+                            <summary className="btn btn-filter w-full">FilterByCategory<IoIosArrowDown /></summary>
+                            <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box">
                                 <li><Link onClick={()=>handleFilterByCategory('Allergy Relief')}>Allergy Relief</Link></li>
                                 <li><Link onClick={()=>handleFilterByCategory('Antibiotics')}>Antibiotics</Link></li>
                                 <li><Link onClick={()=>handleFilterByCategory('Diabetes Care')}>Diabetes Care</Link></li>
@@ -174,12 +174,12 @@ const MedicineSection = () => {
                     </div>  
                 </div> 
                 {/* division----2 */}
-                <div className="grid grid-cols-2 items-center justify-center w-2/3 md:w-full mx-auto">
+                <div className="grid grid-cols-2 gap-1 items-center justify-center w-2/3 md:w-full mx-auto col-span-2">
                     {/* FILTER---BY---PRICE---RANGE---OPTION */}
-                    <div>
-                        <details className="dropdown">
-                            <summary className="btn btn-filter w-40 md:w-full">Filter By Price <IoIosArrowDown /></summary>
-                            <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+                    <div className="w-full">
+                        <details className="dropdown w-full">
+                            <summary className="btn btn-filter w-full">Filter By Price <IoIosArrowDown /></summary>
+                            <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box">
                                 <li><Link onClick={()=>handleFilterByPriceRange(0,5)}><span className="poppins text-yellow-500">0$ to 5$</span></Link></li>
                                 <li><Link onClick={()=>handleFilterByPriceRange(5,10)}><span className="poppins text-yellow-500">5$ to 10$</span></Link></li>
                                 <li><Link onClick={()=>handleFilterByPriceRange(10,15)}><span className="poppins text-yellow-500">10$ to 15$</span></Link></li>
@@ -189,10 +189,10 @@ const MedicineSection = () => {
                         </details>
                     </div>   
                     {/* SORT---OPTION */}
-                    <div>
-                        <details className="dropdown">
-                            <summary className="btn btn-filter w-44 md:w-40">Sort<IoIosArrowDown /></summary>
-                            <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+                    <div className="w-full">
+                        <details className="dropdown w-full">
+                            <summary className="btn btn-filter w-full">Sort<IoIosArrowDown /></summary>
+                            <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box">
                                 <li><Link onClick={sortByLowToHigh}><span className="poppins text-yellow-500">Low to High Price</span></Link></li>
                                 <li><Link onClick={sortByHIghToLow}><span className="poppins text-yellow-500">High to Low Price</span></Link></li>
                                 <li><Link onClick={sortByDateAdded}><span className="poppins text-yellow-500">Addition Date</span></Link></li>
@@ -206,21 +206,18 @@ const MedicineSection = () => {
                 {searchResults.items.map(medicine=> <EachMedicine key={medicine._id} medicine={medicine} ></EachMedicine> )}
             </div>
             <div className="w-1/2 mx-auto text-center mt-6">
-                <p className="mb-2">Page : <span className="text-yellow-500 poppins">{page} </span> , Page Count : <span className="text-yellow-500 poppins">{pageCount}</span></p>
-            </div>
-            <div className="w-5/6 mx-auto text-center mt-6 flex justify-center gap-1">
-                
-                <button disabled={page===1} onClick={handlePrevious} className="btn btn-filter w-full md:w-24">Previous</button>
-                <button disabled={page===pageCount} onClick={handleNext} className="btn btn-filter w-full md:w-28">Next</button>
+                <p className="mb-2"><span className="text-cyan-500">Page : </span> <span className="text-yellow-500 poppins">{page} </span> , 
+                <span className="text-cyan-500">Page Count : </span><span className="text-yellow-500 poppins">{pageCount}</span></p>
             </div>
             {/* <div className="w-5/6 mx-auto text-center mt-6 flex justify-center gap-1">
-                 <button className="btn btn-filter w-16 md:w-20">Previous</button>
-                 <button className="btn btn-round">1</button>
-                 <button className="btn btn-round">2</button>
-                 <button className="btn btn-round">3</button>
-                 <button className="btn btn-round">4</button>
-                <button className="btn btn-filter w-16 md:w-20">Next</button>
+                <button disabled={page===1} onClick={handlePrevious} className="btn btn-filter w-20 md:w-28">Previous</button>
+                <button disabled={page===pageCount} onClick={handleNext} className="btn btn-filter w-20 md:w-28">Next</button>
             </div> */}
+            <div className="w-5/6 mx-auto text-center mt-6 flex justify-center gap-1">
+                 <button disabled={page===1} onClick={handlePrevious} className="btn btn-filter w-16 md:w-20">Previous</button>
+                 {new Array(pageCount).fill(1).map((item,i)=><button onClick={()=>{setPage(i+1)}} key={i} className="btn btn-round">{i+1}</button>)}
+                <button disabled={page===pageCount} onClick={handleNext} className="btn btn-filter w-16 md:w-20">Next</button>
+            </div>
         </div>
     );
 };

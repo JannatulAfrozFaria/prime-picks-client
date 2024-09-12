7622d471e8984e993718fc37b40169ea09f3576d
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-13 01:23:57 +0600

    image added to details page

--- src/pages/Home/MedicineDetails.jsx
+++ src/pages/Home/MedicineDetails.jsx
@@ -7,10 +7,14 @@ const MedicineDetails = () => {
     const {id,image,medicineName,brandName,categoryName,price,dateAdded} = singleMedicine;
     return (
         <div>
+            <div>
+                <img src={image} alt="" />
+            </div>
             <p>Name: {medicineName} </p>
             <p>Category Name: {categoryName} </p>
             <p>Brand Name: {brandName} </p>
             <p>Price: {price} </p>
+            <p>Date of Addition: {dateAdded} </p>
         </div>
     );
 };

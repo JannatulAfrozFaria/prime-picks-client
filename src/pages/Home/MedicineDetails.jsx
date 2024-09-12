715f68094ec0814fffed3be7e9a5f5f8b874bf60
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-13 01:22:44 +0600

    contents added in details page

--- src/pages/Home/MedicineDetails.jsx
+++ src/pages/Home/MedicineDetails.jsx
@@ -4,10 +4,13 @@ import { useLoaderData } from "react-router-dom";
 
 const MedicineDetails = () => {
     const singleMedicine = useLoaderData();
-    const {photo, job, deadline, description,range,number} = singleMedicine;
+    const {id,image,medicineName,brandName,categoryName,price,dateAdded} = singleMedicine;
     return (
         <div>
-            
+            <p>Name: {medicineName} </p>
+            <p>Category Name: {categoryName} </p>
+            <p>Brand Name: {brandName} </p>
+            <p>Price: {price} </p>
         </div>
     );
 };
